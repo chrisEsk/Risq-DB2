3ad0c829d4bad3638abbedcdebcd7e75523484a3
Chris <ceskiv@gmail.com>
2013-08-08 20:24:15 -0600

    Avance 8/AUG

---
+++ Initial Inserts/fill_colonias.sql
@@ -0,0 +1,16 @@
+INSERT INTO Colonias (id_colonia, id_continente, codigo, nombre, x, y, id_equipo)
+VALUES (001, 001, 'ALK', 'Alaska', 0, 0, null);
+INSERT INTO Colonias (id_colonia, id_continente, codigo, nombre, x, y, id_equipo)
+VALUES (002, 001, 'CAN', 'Canada', 1, 0, null);
+INSERT INTO Colonias (id_colonia, id_continente, codigo, nombre, x, y, id_equipo)
+VALUES (003, 001, 'YRK', 'York', 2, 0, null);
+INSERT INTO Colonias (id_colonia, id_continente, codigo, nombre, x, y, id_equipo)
+VALUES (004, 001, 'SEA', 'Seattle', 0, 1, null);
+INSERT INTO Colonias (id_colonia, id_continente, codigo, nombre, x, y, id_equipo)
+VALUES (005, 001, 'ROK', 'Rokies', 1, 1, null);
+INSERT INTO Colonias (id_colonia, id_continente, codigo, nombre, x, y, id_equipo)
+VALUES (006, 001, 'LAX', 'Angeles', 0, 2, null);
+INSERT INTO Colonias (id_colonia, id_continente, codigo, nombre, x, y, id_equipo)
+VALUES (007, 001, 'DAK', 'Dakota', 1, 2, null);
+INSERT INTO Colonias (id_colonia, id_continente, codigo, nombre, x, y, id_equipo)
+VALUES (008, 001, 'MEX', 'Mexico', 1, 3, null);
---
+++ Initial Inserts/fill_continentes.sql
@@ -0,0 +1,12 @@
+INSERT INTO Continentes (id_continente, nombre, valor)
+VALUES (001, 'Nortam', 010);
+INSERT INTO Continentes (id_continente, nombre, valor)
+VALUES (002, 'Latam', 005);
+INSERT INTO Continentes (id_continente, nombre, valor)
+VALUES (003, 'Europas', 011);
+INSERT INTO Continentes (id_continente, nombre, valor)
+VALUES (004, 'Afrikan', 008);
+INSERT INTO Continentes (id_continente, nombre, valor)
+VALUES (005, 'Asia', 014);
+INSERT INTO Continentes (id_continente, nombre, valor)
+VALUES (006, 'Neoland', 004);
