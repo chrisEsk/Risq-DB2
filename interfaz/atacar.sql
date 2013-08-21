bbcd4da771133b288eceb946d96e584b3e95a53f
Andres V <darklink898@gmail.com>
2013-08-20 23:53:08 -0600

    script para realizar ataques
    todavia hacen falta varias cosas, entre ellas, definir la funcion fn_es_vecino

--- interfaz/atacar.sql
+++ interfaz/atacar.sql
@@ -1,26 +1,39 @@
+set serveroutput on;
+
+accept cod_atacante prompt 'Codigo colonia atacante: '
+accept cod_defendiente prompt 'Codigo colonia defendiente: '
+
 DECLARE
     result_atacante dbms_sql.number_table;
     result_defendiente dbms_sql.number_table;
-    cod_atacante varchar2(3) := '&Atacante';
-    cod_defediente varchar2(3) := '&Defendiente';
    	status int;
-   	id_atacante int;
-   	id_defediente int;
+   	id_atacante int := -1;
+   	id_defendiente int := -1;
+   	cod_defendiente varchar2(3);
+   	cod_atacante varchar2(3);
 BEGIN
-
 	select id_colonia into id_atacante
 	from colonias
-	where codigo = cod_atacante;
+	where codigo = '&cod_atacante'
+	and id_equipo = fn_equipo_actual();
 
-	select id_colonia into id_defediente
+	select id_colonia into id_defendiente
 	from colonias
-	where codigo = cod_defediente;
+	where codigo = '&cod_defendiente'
+	and fn_es_vecino(id_colonia, id_atacante) = 1; -- TODO: definir fn_es_vecino
 
     result_atacante := fn_lanzar_dados(id_atacante, 1);
-    result_defendiente := fn_lanzar_dados(id_defediente, 0);
+    result_defendiente := fn_lanzar_dados(id_defendiente, 0);
 
-    EXCEPTION
-		WHEN NO_DATA_FOUND THEN dbms_output.put_line('No data found.');
+    EXCEPTION                 
+        WHEN no_data_found THEN
+            dbms_output.put_line('--ERROR--');
+            IF id_atacante = -1 THEN
+                dbms_output.put_line('No se encontro la colonia atacante');
+            END IF;
 
+            IF id_defendiente = -1 THEN
+                dbms_output.put_line('No se encontro la colonia defendiente');
+            END IF;
 END;
 /
