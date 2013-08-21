61921d7f9a70ea545e1df632ddba79ed491561b5
Andres V <darklink898@gmail.com>
2013-08-20 21:01:39 -0600

    prueba de lanzamiento de dados

--- pruebas/lanzar_dados.sql
+++ pruebas/lanzar_dados.sql
@@ -3,9 +3,10 @@ DECLARE
     result dbms_sql.number_table;
 BEGIN
     result := fn_lanzar_dados(1, 0);
+    dbms_output.put_line('# de dados lanzados: ' || result.count);
     
     FOR i IN 0..result.count-1 LOOP
-        dbms_output.put_line(result(i));
+        dbms_output.put_line('dado #' || (i+1) || ': ' || result(i));
     END LOOP;
 END;
 /
