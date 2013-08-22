5c41e5485d5e66fc3bb3a31e7b19aaae6b7b4123
Andres V <darklink898@gmail.com>
2013-08-21 23:16:00 -0600

    mostrar cant regimientos y comandantes en mapa

--- interfaz/mostrar_mapa.sql
+++ interfaz/mostrar_mapa.sql
@@ -5,20 +5,30 @@ DECLARE
     w int := 8;
     h int := 5;
     w_c int := 10;
+    cant_reg int;
+    cant_com int;
     outp1 varchar2(255) := '';
     outp2 varchar2(255) := '';
     outp3 varchar2(255) := '';
-    CURSOR cur_colonias is select id_colonia,
-    codigo, nombre,
-    x, y, equipos.color as color_equipo
+    CURSOR cur_colonias is
+    select
+        codigo, nombre, id_colonia,
+        x, y, equipos.color as color_equipo
     from colonias
-    left join equipos on colonias.id_equipo = equipos.id_equipo
+    left join equipos
+    on colonias.id_equipo = equipos.id_equipo
     order by y asc, x asc;
 BEGIN
     FOR c IN cur_colonias LOOP
+        select count(id_unidad) into cant_com from unidades
+        where id_tipo_unidad = 1
+        and id_colonia = c.id_colonia;
+        select count(id_unidad) into cant_reg from unidades
+        where id_tipo_unidad = 2
+        and id_colonia = c.id_colonia;
         
-        outp1 := outp1 || rpad('| ' || c.codigo, w_c);
-        outp2 := outp2 || rpad('| ' || c.nombre, w_c);
+        outp1 := outp1 || rpad('| ' || c.nombre, w_c);
+        outp2 := outp2 || rpad('| ' || cant_com || ' ' || cant_reg, w_c);
         outp3 := outp3 || rpad('| ' || c.color_equipo, w_c);
                 
         IF c.x = w-1 THEN
