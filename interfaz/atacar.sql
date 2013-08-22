09f0410294aecdb82d713e1a65a3082e82d0a5b3
Xhanatros <dgom5593@gmail.com>
2013-08-21 23:44:16 -0600

    Script de atacar

--- interfaz/atacar.sql
+++ interfaz/atacar.sql
@@ -11,20 +11,80 @@ DECLARE
    	id_defendiente int := -1;
    	cod_defendiente varchar2(3);
    	cod_atacante varchar2(3);
+    cantidad int := 0;
+    id_equipo_defendiente int;
+    cantidad_restante int;
+    cantidad_atacante int;
+    cantidad_minima int := 2;
 BEGIN
+  -- busca el id de la colonia atacante
 	select id_colonia into id_atacante
 	from colonias
 	where codigo = '&cod_atacante'
 	and id_equipo = fn_equipo_actual();
 
-	select id_colonia into id_defendiente
+  -- busca el id de la colonia defendiente
+	select id_colonia, id_equipo 
+  into id_defendiente, id_equipo_defendiente
 	from colonias
-	where codigo = '&cod_defendiente'
-	and fn_es_vecino(id_colonia, id_atacante) = 1; -- TODO: definir fn_es_vecino
+	where codigo = '&cod_defendiente';
 
-    result_atacante := fn_lanzar_dados(id_atacante, 1);
-    result_defendiente := fn_lanzar_dados(id_defendiente, 0);
+  -- buscar la cantidad de regimientos en la colonia atacante
+  select count(id_unidad)
+  into cantidad_atacante
+  from unidades 
+  where id_colonia = id_atacante;
 
+  -- verifica si la colinia cumple con la cantidad minima para atacar
+  IF cantidad_atacante > cantidad_minima THEN
+  	-- verifica que las colonias sean vecinas
+    IF fn_son_vecinos(id_defendiente, id_atacante) = 1 THEN
+
+          -- verifica que las colonias no sean del mismo equipo
+        IF fn_equipo_actual() != id_equipo_defendiente THEN
+
+            -- se lanzan los dados
+            result_atacante := fn_lanzar_dados(id_atacante, 1);
+            result_defendiente := fn_lanzar_dados(id_defendiente, 0);
+
+            -- determina la menor cantidad de pares a comparar
+            IF result_atacante.count > result_defendiente.count THEN
+              cantidad := result_defendiente.count;
+            ELSE 
+              cantidad := result_atacante.count;
+            END IF;
+
+            -- determina el ganador de cada par y elimina los regimientos
+            FOR i IN 0..cantidad LOOP
+              IF result_atacante(i) > result_defendiente(i) THEN
+                sp_eliminar_unidad(id_defendiente);
+              ELSE
+                sp_eliminar_unidad(id_atacante);
+              END IF;
+            END LOOP;
+            
+            -- busca la cantidad de regimientos restantes de la colonia defendiente
+            select count(*) 
+            into cantidad_restante
+            from colonias 
+            where id_equipo = id_equipo_defendiente;
+
+            -- translada la colonia al atacante
+            IF cantidad_restante = 0 THEN
+              update colonias 
+              set id_equipo = (select id_equipo from colonias where id_colonia = id_atacante)
+              where id_colonia = id_defendiente;
+            END IF;
+      ELSE
+        dbms_output.put_line('Las colonias pertenecen al mismo equipo.');
+      END IF;
+
+    ELSE
+      dbms_output.put_line('Las colonias no son vecinas');
+    END IF;
+  ELSE
+    dbms_output.put_line('La colonia no cumple con la minima cantidad para realizar el ataque.');
+  END IF;
     EXCEPTION                 
         WHEN no_data_found THEN
             dbms_output.put_line('--ERROR--');
