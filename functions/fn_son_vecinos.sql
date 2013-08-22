a8ca39f0b7273f98324f02df7e7e7d5f076dccdf
Andres V <darklink898@gmail.com>
2013-08-21 21:35:49 -0600

    funcion son_vecinos lista

--- functions/fn_son_vecinos.sql
+++ functions/fn_son_vecinos.sql
@@ -1,18 +1,22 @@
-CREATE OR REPLACE FUNCTION fn_son_vecinos(px IN number, py IN varchar2)
+CREATE OR REPLACE FUNCTION fn_son_vecinos(id_colonia1 number, id_colonia2 number)
 RETURN number
 AS
 	v_x number;
 	v_y number;
-	v_es_vecino number default 0;
-Begin
+	v_es_vecino number;
+BEGIN
+    select x, y
+    into v_x, v_y
+    from colonias
+    where id_colonia = id_colonia1;
 
-	select count(id_colonia) INTO v_es_vecino
-	from colonias 
-	where x in (px, px+1, px-1) 
-	and y in (py, py+1, py-1) 
-	and id_colonia != p_id_colonia 
-	and id_colonia = p_id_colonia_2;
-	return es_vecino;
-
-End;
+	select count(id_colonia) into v_es_vecino
+	from colonias
+	where x in (v_x, v_x+1, v_x-1) 
+	and y in (v_y, v_y+1, v_y-1) 
+	and id_colonia != id_colonia1
+	and id_colonia = id_colonia2;
+	
+	return v_es_vecino;
+END;
 /
