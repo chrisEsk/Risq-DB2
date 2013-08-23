c8d3e9dddc38f9dea251565d0e6d0ee3c2673467
Chris <ceskiv@gmail.com>
2013-08-22 22:12:33 -0600

    cambio en asignar reg a colonias

--- interfaz/asignar_regimientos_colonias.sql
+++ interfaz/asignar_regimientos_colonias.sql
@@ -7,27 +7,56 @@ Declare
 	v_idEquipo number;
 	v_idColonia number;
 	v_cantRegActuales number;
+	v_idEquipoFetch number;
+
+	CURSOR query is
+		select id_equipo from colonias;
 Begin
 	-- no mas de 24 reg en total en las colonias de este color
 	select id_equipo into v_idEquipo 
 	from equipos
 	where color = v_color;
 
+	select sum(count(u.id_unidad)) into v_cantRegActuales
+	from equipos e
+	join colonias c on(c.id_equipo = e.id_equipo)
+	join unidades u on(u.id_colonia = c.id_colonia)
+	where e.id_equipo = v_idEquipo
+	group by e.color;
+
 	select id_colonia into v_idColonia
-	from colonias
-	where id_equipo = v_idEquipo;
+	from colonias 
+	where codigo = v_codColonia;
 
-	select count(id_unidad) into v_cantRegActuales
-	from unidades
-	where id_colonia = v_idColonia;
+	if v_cantRegActuales >= v_maxRegimientos OR v_cantRegActuales+v_cantRegimiento >= v_maxRegimientos then
 
-	if v_cantRegActuales > v_maxRegimientos then
 		DBMS_OUTPUT.PUT_LINE('Ya este equipo tiene los regimientos asignados en sus colonias...');
 	else
-		for c in 1..v_cantRegimiento loop
-			INSERT INTO unidades (id_unidad, id_colonia, id_tipo_unidad) VALUES (unidades_seq.nextval, v_idColonia, 1);
-		end loop;
-		DBMS_OUTPUT.PUT_LINE('Se insertaron ' || v_cantRegimiento || ' en la colonia ' || v_codColonia);
+
+		OPEN query;
+		LOOP
+			FETCH query INTO v_idEquipoFetch;
+			
+			if v_idEquipoFetch = v_idEquipo then
+				for c in 1..v_cantRegimiento loop
+					INSERT INTO unidades (id_unidad, id_colonia, id_tipo_unidad) VALUES (unidades_seq.nextval, v_idColonia, 1);
+				end loop;
+			End if;
+			
+		EXIT WHEN query%NOTFOUND;
+		END LOOP;
+		CLOSE query;
+
+		DBMS_OUTPUT.PUT_LINE('El equipo ' || v_color || ' inserto ' || v_cantRegimiento || ' en la colonia ' || v_codColonia);
+
+		select sum(count(u.id_unidad)) into v_cantRegActuales
+		from equipos e
+		join colonias c on(c.id_equipo = e.id_equipo)
+		join unidades u on(u.id_colonia = c.id_colonia)
+		where e.id_equipo = v_idEquipo
+		group by e.color;
+
+		DBMS_OUTPUT.PUT_LINE('El equipo tiene un total de ' || v_cantRegActuales || ' regimientos.');
 	end if;
 End;
 /
